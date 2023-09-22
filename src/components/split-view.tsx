08861282a33c210f198c9e44a9a6625cc065687a
lucas rego <lucasalvesrego.contato@gmail.com>
2023-09-22 18:23:27 -0300

    feat: custom scrollbar

--- src/components/split-view.tsx
+++ src/components/split-view.tsx
@@ -2,10 +2,10 @@ import { Content } from '@/types/content';
 
 export function Splitview({ html, raw }: Pick<Content, 'raw' | 'html'>) {
   return (
-    <div className="w-full h-full flex">
-      <div className="w-1/2 h-full overflow-auto">{raw}</div>
+    <div className="border rounded h-full w-full lg:w-[98vw] lg:h-[80vh] flex lg:flex-row gap-2 p-2 overflow-hidden">
+      <pre className="border rounded p-2 w-full h-1/2 max-h-[50dvh] lg:max-h-screen lg:h-full overflow-auto">{raw}</pre>
       <div
-        className="w-1/2 h-full overflow-auto"
+        className="border rounded p-2 w-full h-1/2 max-h-[50dvh] lg:max-h-screen lg:h-full overflow-auto"
         dangerouslySetInnerHTML={{
           __html: html,
         }}
